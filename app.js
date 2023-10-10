65669897a776414fda47d0b74b7091d033dd8fc6
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-10 14:45:12 +0200

    add carousel Array

---
+++ app.js
@@ -0,0 +1,13 @@
+// # Dato un array contenente una lista di cinque immagini, creare un carosello 
+
+// - creo un Array contenente 5 immagini
+    // - inserisco le immagini fornite all'interno dell' Array
+const carousel = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp"
+]
+
+console.log(carousel)
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Dato un array contenente una lista di cinque immagini, creare un carosello 
+
+- creo un Array contenente 5 immagini
+    - inserisco le immagini fornite all'interno dell' Array
